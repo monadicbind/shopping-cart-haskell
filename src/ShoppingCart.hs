35e93c54cbd5ad13d3152bc46da06e34766a208b
Sathish Mandapaka <comonadicbind@gmail.com>
2019-07-04 19:55:45 +0530

    Different thought process for User Story 4. Might or might not work hence the separate branch.

--- src/ShoppingCart.hs
+++ src/ShoppingCart.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE InstanceSigs #-}
 
 module ShoppingCart
-  ( createAProduct
+  ( createDoveSoapProduct
+  , createAxeDeo
   , createAnEmptyCart
   , numberOfProducts
   , addProducts
@@ -11,13 +12,32 @@ module ShoppingCart
   , TotalPrice(..)
   , TotalPriceWithTax(..)
   , CartPrice(..)
+  , PrdCore(..)
+  , Prd(..)
   ) where
 
 import Data.Decimal
 
+data Offer = BuyXGetYFree
+  { x :: Int
+  , y :: Int
+  } deriving (Show)
+
+data PrdCore = PrdCore
+  { rate :: Decimal
+  , off :: Maybe Offer
+  } deriving (Show)
+
+data Prd
+  = DoveSoap { prd :: PrdCore}
+  | AxeDeo { prd :: PrdCore}
+  | NilProduct { prd :: PrdCore}
+  deriving (Show)
+
 data Product = Product
   { name :: String
   , price :: Decimal
+  , offer :: Maybe Offer
   } deriving (Show)
 
 data TaxAmount = TaxAmount
@@ -38,11 +58,14 @@ data CartPrice = CartPrice
   , getTax :: TaxAmount
   } deriving (Show)
 
-createAProduct :: String -> Decimal -> Product
-createAProduct = Product
+createDoveSoapProduct :: Decimal -> Maybe Offer -> Prd
+createDoveSoapProduct rate offer = DoveSoap (PrdCore rate offer)
+
+createAxeDeo :: Decimal -> Maybe Offer -> Prd
+createAxeDeo rate offer = AxeDeo (PrdCore rate offer)
 
 data Cart = Cart
-  { products :: [Product]
+  { products :: [Prd]
   } deriving (Show)
 
 createAnEmptyCart :: Cart
@@ -51,11 +74,11 @@ createAnEmptyCart = Cart []
 numberOfProducts :: Cart -> Int
 numberOfProducts = length . products
 
-addProducts :: Cart -> Product -> Int -> Cart
+addProducts :: Cart -> Prd -> Int -> Cart
 addProducts cart product quantity = Cart ((products cart) ++ (replicate quantity product))
 
 privateTotalPrice :: Cart -> TotalPrice
-privateTotalPrice cart = TotalPrice $ price (foldl (<>) mempty (products cart))
+privateTotalPrice cart = TotalPrice $ (rate . prd) (foldl (<>) mempty (products cart))
 
 privateTaxAmount :: TotalPrice -> Decimal -> TaxAmount
 privateTaxAmount (TotalPrice totPrc) tax = TaxAmount (roundTo 2 $ (totPrc * tax) / 100)
@@ -67,10 +90,10 @@ totalPriceWithTaxes cart tax =
       tpWithTax = TotalPriceWithTax $ roundTo 2 $ (getTotalPrice tp) + (getTaxAmount taxAmount)
   in CartPrice tpWithTax tp taxAmount
 
-instance Semigroup Product where
-  (<>) :: Product -> Product -> Product
-  (<>) prd1 prd2 = Product "" (price prd1 + price prd2)
+instance Semigroup Prd where
+  (<>) :: Prd -> Prd -> Prd
+  (<>) prd1 prd2 = NilProduct (PrdCore (rate (prd prd1) + rate (prd prd2)) Nothing)
 
-instance Monoid Product where
-  mempty :: Product
-  mempty = Product "" 0.0
+instance Monoid Prd where
+  mempty :: Prd
+  mempty = NilProduct (PrdCore 0.0 Nothing)
